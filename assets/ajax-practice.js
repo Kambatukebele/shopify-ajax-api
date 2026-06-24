ded53d266e49dedb5c752e4a97949b8bbe0c4306
Kambatukebele <kambatukebele@gmail.com>
2026-06-24 14:25:48 +0200

    able to update the cart using section rendering and ajax api

--- assets/ajax-practice.js
+++ assets/ajax-practice.js
@@ -12,8 +12,7 @@ forms.forEach((form) => {
       .querySelector("[data-header-id]")
       .getAttribute("data-header-id");
 
-    form_data.append("sections", get_header_section_id);
-    form_data.append("sections", "cart-drawer");
+    form_data.append("sections", `${get_header_section_id},cart-drawer`);
 
     const res = await fetch(`${URL}cart/add.js`, {
       method: "POST",
@@ -27,17 +26,27 @@ forms.forEach((form) => {
       throw new Error("Something went wront");
     }
     const data = await res.json();
-    console.log(data);
 
     if (data) {
+      // rendering heading to update the bubble count
       const html = document.createElement("div");
       let new_header = data.sections["sections--19986155733179__header"];
-
       html.innerHTML = new_header;
 
       // Updating the bubble
       document.querySelector(".cart-item-bubble > sup").textContent =
         html.querySelector(".cart-item-bubble > sup").textContent;
+
+      // rendering the cart-drawer to update with the new data
+      const cart_drawer_html = document.createElement("section");
+      let new_cart_drawer = data.sections["cart-drawer"];
+
+      cart_drawer_html.innerHTML = new_cart_drawer;
+
+      console.log(cart_drawer_html.innerHTML);
+
+      document.querySelector("[data-drawer-id]").outerHTML =
+        cart_drawer_html.innerHTML;
 
       // Get all the cart item
 
@@ -45,10 +54,10 @@ forms.forEach((form) => {
       // const update_data = await get_updated_cart.json();
       // console.log(update_data);
 
-      // Dispatch update cart
-      //document.dispatchEvent(new CustomEvent("cart:update"));
+      //Dispatch update cart
+      // document.dispatchEvent(new CustomEvent("cart:update"));
       //Dispatch Event open cart
-      //document.dispatchEvent(new CustomEvent("cart:open"));
+      document.dispatchEvent(new CustomEvent("cart:open"));
     }
   });
 });
